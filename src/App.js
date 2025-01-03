b3c858de71a2c4a4724a5438ae0fe7ae748839d2
이지혜 <89819830+jihye1ee@users.noreply.github.com>
2025-01-03 11:36:44 +0900

    강의 파일 업로드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setLoading(false);
+        setCoins(json);
+      });
+  }, []); // 아무것도 주시하지 않으면 이 코드는 한 번만 실행됨
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => <option key={coin.id}>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
+        </select>
+      )}
     </div>
   );
 }
 
 export default App;
+
+/*
+  const [coins, setCoins] = useState();
+  -> 이렇게 기본값을 두고 있지 않으면 {coins.length}가 undefined가 되어 오류가 뜨기 때문에 적어도 비어 있는 array로 두어서 undefined가 되지 않도록 해야 함
+*/
